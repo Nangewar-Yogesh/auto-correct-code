3e7d2ef59ba4067a1f888e2f08c28a384e304d76
hello <yogesh532002@gmail.com>
2025-08-07 14:44:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross-Origin support
+CORS(app)  #Enable Cross-Origin support
 
 @app.route('/predict', methods=['POST'])
 def predict():
